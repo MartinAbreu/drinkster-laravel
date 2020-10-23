51c1b4f1213e2b7351ff7733b167bbe307e97915
Martin Abreu <martin.abreu@live.com>
2020-10-23 14:08:53 -0700

    Fixed bugs in image display

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,12 +6,12 @@
     @foreach ($posts as $post)
     <div class="row w-100">
          <div class="col-6 offset-3 align-items-center pb-2 pt-2">
-         <a href="/p/{{ $post->id }}"><img src="/storage/{{ $post->image }}" class="w-100"></a>
+         <a href="/p/{{ $post->id }}"><img src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" class="w-100"></a>
          </div>
          <div class="col-6 offset-3 pb-2">
              <div class="d-flex align-baseline">
                  <div class="pr-2">
-                     <img src="{{ $post->user->profile->profileImage() }}" class="w-100 rounded-circle" style="max-width: 15px">
+                     <img src="https://drinkster-images.s3-us-west-1.amazonaws.com/profilePic/{{ $user->profile->image }}" class="w-100 rounded-circle" style="max-width: 15px">
                  </div>
                  <div>
                      <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>

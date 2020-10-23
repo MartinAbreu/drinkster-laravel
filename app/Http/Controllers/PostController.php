720bc9f8ca8de1db7be1bb6507ff468218d26b3d
Martin Abreu <martin.abreu@live.com>
2020-10-22 18:40:23 -0700

    AWS S3 configuration

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,7 +70,7 @@ class PostController extends Controller
             'caption' => $data['caption'],
             'ingredients' => $data['ingredients'],
             'instructions' => $data['instructions'],
-            'image' => $storagePath,
+            'image' => $image,
         ]);
 
         return redirect('/profile/' . auth()->user()->id);

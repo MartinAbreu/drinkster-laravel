3afcef099d304e7474ec11a90158a96905f04d19
Martin Abreu <martin.abreu@live.com>
2020-10-23 19:00:07 -0700

    configuring discover page

--- resources/views/posts/discover.blade.php
+++ resources/views/posts/discover.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container w-75">
-   @foreach ($posts as $post)
-   <div class="row w-100">
-    <div class="col-6 offset-3 align-items-center pb-2 pt-2">
-    <a href="/p/{{ $post->id }}"><img src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" class="w-100"></a>
-    </div>
-    <div class="col-6 offset-3 pb-2">
-        <div class="d-flex align-baseline">
-            <div class="pr-2">
-                <img src="https://drinkster-images.s3-us-west-1.amazonaws.com/profilePic/{{ $post->user->profile->image }}" class="w-100 rounded-circle" style="max-width: 15px">
-            </div>
-            <div>
-                <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
+<div class="row pt-5 justify-content-center">
+    @foreach ($posts as $post)
+        <div class="col-md-3 d-flex pb-3 justify-content-center">
+
+            <div class="card">
+                <a href="/p/{{ $post->id }}">
+                    <img class="card-img-top" src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" alt="Card image">
+                </a>
+                <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <p class="card-text">
+                    <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
+                </p>
+                </div>
             </div>
         </div>
-        <hr/>
-    </div>
-</div>
-   @endforeach
+    @endforeach
    <div class="row">
        <div class="col-12 d-flex justify-content-center">
            {{ $posts->links() }}

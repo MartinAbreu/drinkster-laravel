5617a9dfbb5a98bfd9042c9ea57a3483f020a46b
Martin Abreu <martin.abreu@live.com>
2020-10-23 19:23:39 -0700

    configuring discover page

--- resources/views/posts/discover.blade.php
+++ resources/views/posts/discover.blade.php
@@ -9,7 +9,7 @@
             <div class="card">
                     <img class="card-img-top" src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" alt="Card image">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <h5 class="card-title text-dark">{{ $post->title }}</h5>
                         <p class="card-text">
                             <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
                         </p>

4c192675cd392d91f48b9725bff59cc6be0e9f99
Martin Abreu <martin.abreu@live.com>
2020-10-25 13:14:40 -0700

    Tweaked styling a bit

--- resources/views/posts/discover.blade.php
+++ resources/views/posts/discover.blade.php
@@ -1,27 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row pt-5 justify-content-center">
-    @foreach ($posts as $post)
-        <div class="col-md-3 d-flex pb-3 justify-content-center">
+<div class="container">
+    <div class="row pt-5 justify-content-center">
+        @foreach ($posts as $post)
+            <div class="col-md-3 d-flex pb-3 justify-content-center">
 
-            <a href="/p/{{ $post->id }}">
-            <div class="card">
-                    <img class="card-img-top" src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title text-dark">{{ $post->title }}</h5>
-                        <p class="card-text">
-                            <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
-                        </p>
+                <a href="/p/{{ $post->id }}">
+                <div class="card">
+                        <img class="card-img-top" src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" alt="Card image">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark">{{ $post->title }}</h5>
+                            <p class="card-text">
+                                <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
-    @endforeach
-   <div class="row">
-       <div class="col-12 d-flex justify-content-center">
-           {{ $posts->links() }}
+                </a>
+            </div>
+        @endforeach
+       <div class="row">
+           <div class="col-12 d-flex justify-content-center">
+               {{ $posts->links() }}
+           </div>
        </div>
-   </div>
+    </div>
 </div>
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,30 +2,32 @@
 
 @section('content')
 @if ($userFollowing)
-<div class="row pt-5 justify-content-center">
-    @foreach ($posts as $post)
+<div class="container">
+    <div class="row pt-5 justify-content-center">
+        @foreach ($posts as $post)
 
-    <div class="col-md-3 d-flex pb-3 justify-content-center">
+        <div class="col-md-3 d-flex pb-3 justify-content-center">
 
-        <div class="card">
-            <a href="/p/{{ $post->id }}">
-                <img class="card-img-top" src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" alt="Card image">
-            </a>
-            <div class="card-body">
-            <h5 class="card-title">{{ $post->title }}</h5>
-              <p class="card-text">
-                  <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
-              </p>
+            <div class="card">
+                <a href="/p/{{ $post->id }}">
+                    <img class="card-img-top" src="https://drinkster-images.s3-us-west-1.amazonaws.com/uploads/{{ $post->image }}" alt="Card image">
+                </a>
+                <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                  <p class="card-text">
+                      <span><a href="/profile/{{ $post->user->profile->user_id }}"><strong><span class="text-dark pr-1">{{ $post->user->username }}</span></strong> </a>{{ $post->caption }}</span>
+                  </p>
+                </div>
             </div>
         </div>
-    </div>
-    @endforeach
-    <div class="row">
-        <div class="col-12 d-flex justify-content-center">
-            {{ $posts->links() }}
+        @endforeach
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                {{ $posts->links() }}
+            </div>
         </div>
-    </div>
- </div>
+     </div>
+</div>
 @else
     <div class="container w-100">
         <div class="jumbotron">
